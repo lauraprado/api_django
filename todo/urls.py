c5e9711230013469eb8daf5def400a1f988400da
Laura Prado Marchal <laura.prado.marchal@gmail.com>
2023-05-09 12:10:57 +0200

    project complete

---
+++ todo/urls.py
@@ -0,0 +1,40 @@
+"""todo URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Panel comercial",
+        default_version='v1',
+        description='API Panel Comercial',
+        ),
+    public=True,
+    permission_classes=(permissions.IsAdminUser,),
+    )
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # path('api-auth/', include('rest_framework.urls')),
+    path('swagger', schema_view.with_ui('swagger', cache_timeout = 0), name="schema-swagger-ui"),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/v1/auth/', include('rest_auth.urls')),
+    path('api/v1/auth/registration/', include('rest_auth.registration.urls')),
+    path('todos/', include('todo_api.urls')),
+]
